d30a240e75b9a5b9f135ee8bd520770e765543d7
“Douglas <dsena.m@hotmail.com>
2023-02-04 23:21:10 -0300

    feat: config theme

---
+++ src/styles/theme/index.js
@@ -0,0 +1,12 @@
+import { extendTheme } from "native-base";
+
+export const theme = extendTheme({
+  colors: {
+    // Add new color
+    primary: {
+      500: "#ff6347",
+    },
+    
+  },
+  
+});
